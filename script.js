c541b228e7d166b0acb3701a13d0b48e8d1c9c39
aljaz <aljaz.berglez@gmail.com>
2020-12-21 12:04:21 +0100

    init

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const btns = document.querySelectorAll(".buttons button");
 const links = document.querySelectorAll(".home-box-3 div");
 const burger = document.querySelector(".burger");
 const bars = document.querySelectorAll(".burger div");
+const homePage = document.querySelector("#home");
+const productBtn = document.querySelector(".product-btn")
 
 
 // Main Function
@@ -17,15 +19,17 @@ const main = (e) => {
     const gsapAnimations = () => {
         let tl = gsap.timeline();
         tl.from(logo, {duration: 1.5, opacity: 0, x: -50, y: -50});
-        tl.from(logoText, {duration: 1, opacity: 0, stagger: 0.2});
-        tl.from(shortText, {duration: 1, opacity: 0, x: 50});
-        tl.from(btns, {duration: 1, opacity: 0, stagger: 0.25});
-        tl.from(links, {duration: 1, opacity: 0, stagger: 0.25, y: 50});
+        tl.from(logoText, {duration: 1, opacity: 0, stagger: 0.1});
+        tl.from(shortText, {duration: 1, opacity: 0});
+        tl.from(btns, {duration: 0.5, opacity: 0, stagger: 0.25});
+        tl.from(links, {duration: 0.5, opacity: 0, stagger: 0.25, y: 50});
+        tl.to(homePage, {duration: 1, minHeight: "85vh"});
     }
 
     // Calling functions
-    gsapAnimations();
-
+    if (window.scrollY < 350){
+        gsapAnimations();
+    }
 }
 
 
