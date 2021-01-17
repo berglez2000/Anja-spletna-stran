3e99890edb0b150b3c387219c5b5a0d9b67b94b6
aljaz <aljaz.berglez@gmail.com>
2021-01-17 15:52:18 +0100

    init

--- products/dig-foto/script.js
+++ products/dig-foto/script.js
@@ -1,5 +1,5 @@
 let currentPhoto = 0;
-let photosNum = 4;
+let photosNum = 6;
 
 // Query Selectors
 const largeImage = document.querySelector("#show-img");
---
+++ products/skruncie/script.js
@@ -0,0 +1,47 @@
+let currentPhoto = 0;
+let photosNum = 3;
+
+// Query Selectors
+const largeImage = document.querySelector("#show-img");
+const smallImages = document.querySelectorAll(".small-images img");
+const arrowForward = document.querySelector("#arrow-forward");
+const arrowbackward = document.querySelector("#arrow-backward");
+
+
+
+// Functions
+const slideImage = () => {
+  if (currentPhoto > photosNum){
+    currentPhoto = 0;
+  } else if (currentPhoto < 0){
+    currentPhoto = photosNum;
+  }
+
+  largeImage.src = `img/img-${currentPhoto}.jpg`;
+}
+
+
+// Event Listeners
+arrowForward.addEventListener("click", () => {
+  currentPhoto++;
+  slideImage();
+});
+
+arrowbackward.addEventListener("click", () => {
+  currentPhoto--;
+  slideImage();
+});
+
+const changeImages = (img) => {
+  let num = img.alt;
+
+  currentPhoto = num;
+  return largeImage.src = `img/img-${num}.jpg`;
+}
+
+// For each loop
+smallImages.forEach(img => {
+  img.addEventListener("click", () => {
+    changeImages(img);
+  });
+});
